e745050e702abd2651d9a6fddc8edd6811f9c1ce
se62-08 <sopon0714@gmail.com>
2020-06-04 18:02:25 +0700

    update2

--- testProject/manage.php
+++ testProject/manage.php
@@ -14,7 +14,27 @@ switch ($action) {
         $DID = $_POST['DID'] ?? "";
         $SPID = $_POST['SPID'] ?? "";
         $PID = $_POST['PID'] ?? "";
-        $sql = "DELETE FROM working WHERE DID = $DID AND SPID =$SPID  AND PID= $PID";
+        $sql = "INSERT INTO `working` (`WID`, `DID`, `SPID`, `PID`) VALUES (NULL, '$DID', '$SPID', '$PID')";
         deletedata($sql);
+        break;
+    case "ClearWorking":
+        $sql = "DELETE FROM working ";
+        deletedata($sql);
+        break;
+    case "getTextSelect":
+        $DID = $_POST['DID'] ?? "";
+        $SPID = $_POST['SPID'] ?? "";
+        $PID = $_POST['PID'] ?? "";
+        $NAMEPEOPLE = getNamePeople($DID, $SPID, $PID);
+        $text = "<option value=\"0\">เลือกรายชื่อ</option>";
+        for ($i = 1; $i < count($NAMEPEOPLE); $i++) {
+            $text .= "<option value=\"{$NAMEPEOPLE[$i]['PID']}\" ";
+            if ($NAMEPEOPLE[$i]['PID'] == $PID) {
+                $text .= " selected ";
+            }
+            $text .= ">{$NAMEPEOPLE[$i]['PName']}</option>";
+        }
+        echo $text;
+
         break;
 }
--- testProject/query.php
+++ testProject/query.php
@@ -1,19 +1,12 @@
 <?php
 include_once("../dbConnect.php");
 //////////////// ดึงข้อมูลรายชื่อทั้งหมด
-function getNamePeople()
+function getNamePeople($DID = 1, $SPID = 0, $PID = 0)
 {
-    $DID = $_POST['DID'] ?? "1";
-    $SPID = $_POST['SPID'] ?? "0";
-    $PID = $_POST['PID'] ?? "0";
-    $isJson = $_POST['isJson'] ?? false;
     $sql = "SELECT * FROM `people`  WHERE `people`.`PID` NOT IN (SELECT `working`.`PID` FROM `working` 
     INNER JOIN `servicepoint` ON `servicepoint`.`SPID`=`working`.`SPID`
     WHERE  `working`.`DID` != $DID OR ( `working`.`DID` = $DID AND `servicepoint`.`SPID` = $SPID)) 
-     OR  `people`.`PID`=$PID";
+     OR  `people`.`PID`=$PID ORDER BY PName";
     $INFONAME = selectData($sql);
-    if ($isJson) {
-        echo json_encode($INFONAME);
-    }
     return   $INFONAME;
 }
