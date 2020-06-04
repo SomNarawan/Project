aec51e02d4dc1e42dbdef65dd9b7e0ef7b095cfc
SomNarawan <lovesom2112@hotmail.com>
2020-06-04 14:07:23 +0700

    db

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <div class="row">
+        <div class="col-lg-3">
+        </div>
+        <div class="col-lg-3">
+            วันที่ตรวจ 
+            <input type="date">
+        </div>
+        <div class="col-lg-3">
+            เวลาตรวจ 
+            <input type="time">
+        </div>
+        <div class="col-lg-3">
+            เวลารถออก 
+            <input type="time">
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-3">
+            ชื่อบริษัท 
+            <input type="text">
+        </div>
+        <div>
+            จังหวัด 
+            <input type="text">
+        </div>
+        <div>
+            จำนวนบริษัท 
+            <input type="number">
+            <button>ตกลง</button>
+        </div>
+    </div>
+
+</body>
+</html>
