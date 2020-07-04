5a1c52f52573f251cc620d7ec0eabc4b07622d16
se62-08 <sopon0714@gmail.com>
2020-07-04 15:09:04 +0700

    ีupdate

--- vehicle.php
+++ vehicle.php
@@ -47,42 +47,36 @@ $VEHICLE = getVehicle();
                     </thead>
                     <?php
                     for ($i = 1; $i < count($VEHICLE); $i++) {
-                        if($VEHICLE[$i]['statusVehicle'] == "พร้อมใช้งาน"){
+                        if ($VEHICLE[$i]['statusVehicle'] == "พร้อมใช้งาน") {
                             $colorstatus = 'green';
-                        }else{
+                        } else {
                             $colorstatus = 'red';
                         }
-                        
-                        if($VEHICLE[$i]['comment'] == null){
+
+                        if ($VEHICLE[$i]['comment'] == null) {
                             $comment = '-';
-                        }else{
+                        } else {
                             $comment = $VEHICLE[$i]['comment'];
                         }
-                        ?>
+                    ?>
 
-                    <tr align="center" name="head_table">
-                        <td><?php echo $i; ?></th>
-                        <td style="text-align: left;"><?php echo $VEHICLE[$i]['VName']; ?></td>
-                        <td style="text-align: center;color: <?php echo $colorstatus; ?>;">
-                            <?php echo $VEHICLE[$i]['statusVehicle']; ?>
-                        </td>
-                        <td style="text-align: center;"><?php echo $comment; ?></td>
-                        <td>
-                            <button type=button" class="btn btn-warning btn-sm btn_edit tt"
-                                vid="<?php echo $VEHICLE[$i]['VID']; ?>" vname="<?php echo $VEHICLE[$i]['VName']; ?>"
-                                vstatus="<?php echo $VEHICLE[$i]['statusVehicle']; ?>" 
-                                vnote="<?php echo $VEHICLE[$i]['comment']; ?>" data-toggle="tooltip"
-                                title="แก้ไขข้อมูล">
-                                <i class="fa fa-pencil" aria-hidden="true"></i>
-                            </button>
-                            <button type="button" class="btn btn-danger btn-sm btn_del tt"
-                                vid="<?php echo $VEHICLE[$i]['VID']; ?>" vname="<?php echo $VEHICLE[$i]['VName']; ?>"
-                                data-toggle="tooltip" title="ลบ">
-                                <i class="fa fa-trash" aria-hidden="true"></i>
-                            </button>
-                        </td>
-                    </tr>
-                    <?php 
+                        <tr align="center" name="head_table">
+                            <td><?php echo $i; ?></th>
+                            <td style="text-align: left;"><?php echo $VEHICLE[$i]['VName']; ?></td>
+                            <td style="text-align: center;color: <?php echo $colorstatus; ?>;">
+                                <?php echo $VEHICLE[$i]['statusVehicle']; ?>
+                            </td>
+                            <td style="text-align: center;"><?php echo $comment; ?></td>
+                            <td>
+                                <button type=button" class="btn btn-warning btn-sm btn_edit tt" vid="<?php echo $VEHICLE[$i]['VID']; ?>" vname="<?php echo $VEHICLE[$i]['VName']; ?>" vstatus="<?php echo $VEHICLE[$i]['statusVehicle']; ?>" vnote="<?php echo $VEHICLE[$i]['comment']; ?>" data-toggle="tooltip" title="แก้ไขข้อมูล">
+                                    <i class="fa fa-pencil" aria-hidden="true"></i>
+                                </button>
+                                <button type="button" class="btn btn-danger btn-sm btn_del tt" vid="<?php echo $VEHICLE[$i]['VID']; ?>" vname="<?php echo $VEHICLE[$i]['VName']; ?>" data-toggle="tooltip" title="ลบ">
+                                    <i class="fa fa-trash" aria-hidden="true"></i>
+                                </button>
+                            </td>
+                        </tr>
+                    <?php
                     }
                     ?>
                 </table>
@@ -110,8 +104,7 @@ $VEHICLE = getVehicle();
                                 <span>รายชื่อยานพาหนะ</span>
                             </div>
                             <div class="col-xl-5 col-12">
-                                <input type="text" class="form-control" name="name" required=""
-                                    placeholder="กรุณากรอกชื่อยานพาหนะ">
+                                <input type="text" class="form-control" name="name" required="" placeholder="กรุณากรอกชื่อยานพาหนะ">
                             </div>
                         </div>
                         <div class="row mb-4">
@@ -120,8 +113,7 @@ $VEHICLE = getVehicle();
                             </div>
                             <div class="col-xl-5 col-12">
                                 <input type="radio" name="status" id="status1" required="" value="พร้อมใช้งาน"> พร้อมใช้งาน
-                                <input type="radio" style="margin-left:20%" name="status" id="status2" required=""
-                                    value="ไม่พร้อมใช้งาน"> ไม้พร้อมใช้งาน
+                                <input type="radio" style="margin-left:20%" name="status" id="status2" required="" value="ไม่พร้อมใช้งาน"> ไม้พร้อมใช้งาน
                             </div>
                         </div>
                         <div class="row mb-4">
@@ -129,7 +121,7 @@ $VEHICLE = getVehicle();
                                 <span>หมายเหตุ</span>
                             </div>
                             <div class="col-xl-5 col-12">
-                                <textarea class="form-control" placeholder="หมายเหตุ" name="note" id="note" rows="3" cols="39" ></textarea>
+                                <textarea class="form-control" placeholder="หมายเหตุ" name="note" id="note" rows="3" cols="39"></textarea>
                             </div>
                         </div>
                         <input type="hidden" name="action" value="addvehicle">
@@ -160,8 +152,7 @@ $VEHICLE = getVehicle();
                                 <span>รายชื่อยานพาหนะ</span>
                             </div>
                             <div class="col-xl-5 col-12">
-                                <input type="text" class="form-control" name="e_name" id="e_name" required=""
-                                    placeholder="กรุณากรอกชื่อยานพาหนะ">
+                                <input type="text" class="form-control" name="e_name" id="e_name" required="" placeholder="กรุณากรอกชื่อยานพาหนะ">
                             </div>
                         </div>
                         <div class="row mb-4">
@@ -171,8 +162,7 @@ $VEHICLE = getVehicle();
                             <div class="col-xl-5 col-12">
                                 <input type="radio" name="e_status" id="e_status1" required="" value="พร้อมใช้งาน">
                                 พร้อมใช้งาน
-                                <input type="radio" style="margin-left:20%" name="e_status" id="e_status2" required=""
-                                    value="ไม่พร้อมใช้งาน"> ไม้พร้อมใช้งาน
+                                <input type="radio" style="margin-left:20%" name="e_status" id="e_status2" required="" value="ไม่พร้อมใช้งาน"> ไม้พร้อมใช้งาน
                             </div>
                         </div>
                         <div class="row mb-4">
@@ -180,7 +170,7 @@ $VEHICLE = getVehicle();
                                 <span>หมายเหตุ</span>
                             </div>
                             <div class="col-xl-5 col-12">
-                                <textarea class="form-control" placeholder="หมายเหตุ" name="e_note" id="e_note" rows="3" cols="39" ></textarea>
+                                <textarea class="form-control" placeholder="หมายเหตุ" name="e_note" id="e_note" rows="3" cols="39"></textarea>
                             </div>
                         </div>
                         <input type="hidden" name="VID" id="VID" value="0">
@@ -196,87 +186,87 @@ $VEHICLE = getVehicle();
     </form>
 </div>
 <script>
-$(document).ready(function() {
-    $('.tt').tooltip({
-        trigger: "hover"
-    });
-    $('.datatables').DataTable();
+    $(document).ready(function() {
+        $('.tt').tooltip({
+            trigger: "hover"
+        });
+        $('.datatables').DataTable();
 
-    $(document).on("click", "#btn_add", function() {
-        $("#addModal").modal();
-    });
-    $(document).on("click", "#status1", function() {
-        console.log('status1');
-        $("#note").attr('disabled', 'disabled');
-    });
-    $(document).on("click", "#status2", function() {
-        $("#note").removeAttr('disabled');
-    });
-    $(document).on("click", "#e_status1", function() {
-        $("#e_note").attr('disabled', 'disabled');
-    });
-    $(document).on("click", "#e_status2", function() {
-        $("#e_note").removeAttr('disabled');
-    });
-    $(document).on("click", ".btn_edit", function() {
-        var VID = $(this).attr('vid');
-        var VName = $(this).attr('vname');
-        var VStatus = $(this).attr('vstatus');
-        var VNote = $(this).attr('vnote');
-
-        $("#VID").val(VID);
-        $("#e_name").val(VName);
-        $("#e_status").val(VStatus);
-        if (VStatus == 'พร้อมใช้งาน') {
-            $("#e_status1").attr("checked", "checked");
+        $(document).on("click", "#btn_add", function() {
+            $("#addModal").modal();
+        });
+        $(document).on("click", "#status1", function() {
+            console.log('status1');
+            $("#note").attr('disabled', 'disabled');
+        });
+        $(document).on("click", "#status2", function() {
+            $("#note").removeAttr('disabled');
+        });
+        $(document).on("click", "#e_status1", function() {
             $("#e_note").attr('disabled', 'disabled');
-        } else {
-            $("#e_status2").attr("checked", "checked");
+        });
+        $(document).on("click", "#e_status2", function() {
             $("#e_note").removeAttr('disabled');
-        }
-        $("#e_note").val(VNote);
-        $("#editModal").modal();
-    });
-    $(document).on("click", ".btn_del", function() {
-        var VID = $(this).attr('vid');
-        var VName = $(this).attr('vname');
-        swal({
-                title: "คุณต้องการลบ",
-                text: `ยานพาหนะ ${VName} หรือไม่ ?`,
-                icon: "warning",
-                confirmButtonClass: "btn-danger",
-                cancelButtonClass: "btn-secondary",
-                confirmButtonText: "ยืนยัน",
-                cancelButtonText: "ยกเลิก",
-                buttons: true,
-                dangerMode: true,
-            })
-            .then((willDelete) => {
-                if (willDelete) {
-                    swal("ลบข้อมูลสำเร็จ", {
-                        icon: "success",
-                    }).then((confirm) => {
-                        if (confirm) {
-                            delete_1(VID);
-                        }
-                    });
-                } else {
+        });
+        $(document).on("click", ".btn_edit", function() {
+            var VID = $(this).attr('vid');
+            var VName = $(this).attr('vname');
+            var VStatus = $(this).attr('vstatus');
+            var VNote = $(this).attr('vnote');
 
-                }
-            });
-
-    });
-
-    function delete_1(VID) {
-        var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                window.location = './vehicle.php';
+            $("#VID").val(VID);
+            $("#e_name").val(VName);
+            $("#e_status").val(VStatus);
+            if (VStatus == 'พร้อมใช้งาน') {
+                $("#e_status1").attr("checked", "checked");
+                $("#e_note").attr('disabled', 'disabled');
+            } else {
+                $("#e_status2").attr("checked", "checked");
+                $("#e_note").removeAttr('disabled');
             }
-        };
-        xhttp.open("POST", "manage.php", true);
-        xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-        xhttp.send(`VID=${VID}&action=deletevehicle`);
-    }
-});
+            $("#e_note").val(VNote);
+            $("#editModal").modal();
+        });
+        $(document).on("click", ".btn_del", function() {
+            var VID = $(this).attr('vid');
+            var VName = $(this).attr('vname');
+            swal({
+                    title: "คุณต้องการลบ",
+                    text: `ยานพาหนะ ${VName} หรือไม่ ?`,
+                    icon: "warning",
+                    confirmButtonClass: "btn-danger",
+                    cancelButtonClass: "btn-secondary",
+                    confirmButtonText: "ยืนยัน",
+                    cancelButtonText: "ยกเลิก",
+                    buttons: true,
+                    dangerMode: true,
+                })
+                .then((willDelete) => {
+                    if (willDelete) {
+                        swal("ลบข้อมูลสำเร็จ", {
+                            icon: "success",
+                        }).then((confirm) => {
+                            if (confirm) {
+                                delete_1(VID);
+                            }
+                        });
+                    } else {
+
+                    }
+                });
+
+        });
+
+        function delete_1(VID) {
+            var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200) {
+                    window.location = './vehicle.php';
+                }
+            };
+            xhttp.open("POST", "manage.php", true);
+            xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+            xhttp.send(`VID=${VID}&action=deletevehicle`);
+        }
+    });
 </script>
